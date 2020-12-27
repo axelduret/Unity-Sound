8e70bcf84cbc4ebdb380b737f8b87e023fd8f2a9
Axel Duret <axelfaya@gmail.com>
2020-12-27 19:23:17 +0100

    add enter key event on admin video page

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,12 +1,25 @@
 /* Start Custom JQuery Functions */
 (function ($) {
 
+  /* Video page */
   /* Video pause when closing modal */
   $('.modal').on('hidden.bs.modal', function (_e) {
     $iframe = $(this).find("iframe");
     $iframe.attr("src", $iframe.attr("src"));
   });
 
+  /* Video admin page */
+  /* Open modal on enter key press */
+  if ($("#previewButton").length) {
+    $('input').on('keypress', (event) => {
+      if (event.which === 13) {
+        event.preventDefault();
+        $('#previewButton').click();
+      }
+    });
+  }
+
+  /* Video admin page */
   /* Display form content inside modal when clicking on preview button */
   $("#previewButton").on('click', function (_e) {
     if ($("#title").val() != "" && $("#episode").val() != "" && $("#date").val() != "" && $("#artist").val() != "" && $("#url").val() != "") {
@@ -36,6 +49,7 @@
 })(jQuery);
 /* End Custom JQuery Functions */
 
+/* Login and register page */
 /* Recaptcha Callback */
 function onSubmit(token) {
   document.getElementById("validate-form").submit()
