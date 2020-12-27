780709e73f46ad17b733409ed815f57a51aa29b0
Axel Duret <axelfaya@gmail.com>
2020-12-27 15:24:18 +0100

    edit account page

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,10 +1,40 @@
-/* Video pause when modal close */
+/* Start Custom JQuery Functions */
 (function ($) {
+
+  /* Video pause when closing modal */
   $('.modal').on('hidden.bs.modal', function (_e) {
     $iframe = $(this).find("iframe");
     $iframe.attr("src", $iframe.attr("src"));
   });
+
+  /* Display form content inside modal when clicking on preview button */
+  $("#previewButton").on('click', function (_e) {
+    if ($("#title").val() != "" && $("#episode").val() != "" && $("#date").val() != "" && $("#artist").val() != "" && $("#url").val() != "") {
+      $("#previewModal").modal('show');
+      $("#previewModal").modal(
+        $("#videoTitle").html(
+          $("#title").val()
+        ) +
+        $("#videoEpisode").html(
+          $("#episode").val()
+        ) +
+        $("#videoDate").html(
+          $("#date").val()
+        ) +
+        $("#videoArtist").html(
+          "Feat. " + $("#artist").val()
+        ) +
+        $("#videoUrl").attr("src",
+          "https://img.youtube.com/vi/" + $("#url").val() + "/hqdefault.jpg"
+        )
+      );
+    } else {
+      alert("Remplir tous les champs avant de valider");
+    }
+  });
+
 })(jQuery);
+/* End Custom JQuery Functions */
 
 /* Recaptcha Callback */
 function onSubmit(token) {
